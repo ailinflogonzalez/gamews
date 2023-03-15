10455f831606756dea6d3cede9891fd0354fd45f
ailinflogonzalez <ailinflogonzalez@gmail.com>
2023-03-15 12:06:45 -0300

    organización de folders

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const options = {
   }
 }
 
-fetch('https://mmo-games.p.rapidapi.com/latestnews', options)
+/* fetch('https://mmo-games.p.rapidapi.com/latestnews', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err))
@@ -21,26 +21,26 @@ fetch('https://mmo-games.p.rapidapi.com/latestnews', options)
     console.log(response[1].source)
     console.log(response[2].title)
     console.log(response[2].source)
-    console.log(response)
+    console.log(response) */
 
-    /* NOTICIA 1 */
-    document.getElementById('text_0').innerHTML = response[0].title;
+/* NOTICIA 1 */
+/*     document.getElementById('text_0').innerHTML = response[0].title;
     document.getElementById('source_0').innerHTML = '--- ' + response[0].source + ' ---';
-    document.getElementById('url_0').innerHTML = response[0].article_url;
-    /* FIN NOTICIA 1 */
+    document.getElementById('url_0').innerHTML = response[0].article_url; */
+/* FIN NOTICIA 1 */
 
-    /* NOTICIA 2 */
-    document.getElementById('text_1').innerHTML = response[1].title;
+/* NOTICIA 2 */
+/*     document.getElementById('text_1').innerHTML = response[1].title;
     document.getElementById('source_1').innerHTML = '--- ' + response[1].source + ' ---';
-    document.getElementById('url_1').innerHTML = response[1].article_url;
-    /* FIN NOTICIA 2 */
+    document.getElementById('url_1').innerHTML = response[1].article_url; */
+/* FIN NOTICIA 2 */
 
-    /* NOTICIA 3 */
-    document.getElementById('text_2').innerHTML = response[2].title;
+/* NOTICIA 3 */
+/*     document.getElementById('text_2').innerHTML = response[2].title;
     document.getElementById('source_2').innerHTML = '--- ' + response[2].source + ' ---';
     document.getElementById('url_2').innerHTML = response[2].article_url;
-    /* FIN NOTICIA 3 */
+    }) */
+/* FIN NOTICIA 3 */
 
-  })
 
 /* AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA */
